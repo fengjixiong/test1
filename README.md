74e0794d82ac30ee6fb660742734db98445f89f3
fengjx <vonseashore@163.com>
2018-09-01 14:53:47 +0800

    [modify] script. but it's useless

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test1
 just for test
+
+lib.exe可以直接把所有dll导出为lib
--- script/generate_dll_exports.py
+++ script/generate_dll_exports.py
@@ -1,4 +1,4 @@
-import sys,os
+﻿import sys,os
 
 header_str = '''
 #ifndef _%s
@@ -6,8 +6,8 @@ header_str = '''
 
 #include "my_dll_exports.h"\n
 '''
-prefix_str = 'DLL_API'
-define_str = 'DLL_EXPORTS'
+prefix_str = 'DLL_API '
+define_str = '#define DLL_EXPORTS 1\n'
 
 def parse_cpp(filename):
 	f = open(filename, 'r')
@@ -17,8 +17,25 @@ def parse_cpp(filename):
 	f.write(define_str)
 	f.write(buffer)
 	f.close()
+	
+	name = filename.split('/')[-1].split('.')[0]
+	fdir = '/'.join(filename.split('/')[0:-1])
+	os.system('mv %s %s.cbak'%(filename, fdir + '/' + name))
+	os.system('mv %s %s.cpp'%(filename+'.ctemp', fdir + '/' + name))
+	
 	pass
 
+def parse_line(line):
+	if len(line) == 0:
+		line = line
+	elif not (line[0].isalpha() or line[0] == '_'):
+		line = line
+	elif line.startswith('class ') or line.startswith('template ') or line.startswith('struct ') or line.startswith('typedef struct'):
+		line = prefix_str + line
+	elif line.find('(') != -1 and line.find(')') != -1 and (line.endswith(';') or line.strip().endswith(',')):
+		line = prefix_str + line
+	return line
+	
 def parse_h(filename):
 	'给.h文件添加my_dll_exports.h'
 	'同时给每个类声明添加DLL_API'
@@ -29,12 +46,15 @@ def parse_h(filename):
 	f = open(filename +'.htemp', 'w')
 	f.write(header_str%(fname.upper(),fname.upper()))
 	for line in buffer.split('\n'):
-		if line.starswith('class ') or \
-		line.starswith('template '):
-			line = prefix_str + line
-		f.write(line)
+		line = parse_line(line)
+		f.write(line + '\n')
 	f.write('\n#endif\n')
 	f.close()
+	
+	name = filename.split('/')[-1].split('.')[0]
+	fdir = '/'.join(filename.split('/')[0:-1])
+	os.system('mv %s %s.hbak'%(filename, fdir + '/' + name))
+	os.system('mv %s %s.h'%(filename+'.htemp', fdir + '/' + name))
 	
 	pass
 
@@ -43,15 +63,16 @@ def main(libdir):
 	dirs = os.walk(libdir)
 	for [curr_dir,sub_dir,files] in dirs:
 		for f in files:
-			fullname = libdir + curr_dir + f
-			if f.endswith('.h'):
+			fullname = curr_dir + '/' + f
+			print(fullname)
+			if f.endswith('.hbak'):
 				parse_h(fullname)
-			elif f.endswith('.cpp'):
+			elif f.endswith('.cbak'):
 				parse_cpp(fullname)
 	pass
 
 if __name__ == '__main__':
 	if len(sys.argv) != 2:
 		print('usage: python generate_dll_exports.py bdir')
-		return
-	main(sys.argv[1])
+	else:
+		main(sys.argv[1])
